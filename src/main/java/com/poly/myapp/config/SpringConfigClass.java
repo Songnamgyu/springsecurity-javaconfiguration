20fd14cc73008322d5e3877d185c059e24386676
Songnamgyu <namgyu1020@naver.com>
2021-07-03 16:54:53 +0900

    sprng security javaconfiguration 시작

---
+++ src/main/java/com/poly/myapp/config/SpringConfigClass.java
@@ -0,0 +1,10 @@
+package com.poly.myapp.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class SpringConfigClass implements WebApplicationInitializer{
+
+	//web.xml 역할
+	
+	
+}
